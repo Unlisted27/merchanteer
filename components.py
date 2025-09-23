ff8924036e988f2f3560c45b30c12c417ed61371
Unlisted_dev <archerhpo@gmail.com>
2025-09-23 19:11:58 -0300

    Final commit before adding storage component to ships

--- components.py
+++ components.py
@@ -1,4 +1,9 @@
-def menu(name:str,options:list,horizontal_sign="_",vertical_sign="|",return_tuple:bool=False):
+import os
+
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def menu(name:str,list_options:list,return_option=False,horizontal_sign="_",vertical_sign="|",return_tuple:bool=False):
     '''Displays a menu of the paramater options.
     -
     Selected option returned as string
@@ -11,7 +16,10 @@ def menu(name:str,options:list,horizontal_sign="_",vertical_sign="|",return_tupl
         ex: in the previous example, (1,"a") would be returned'''
     length = 1
     i=1
+    options = list_options.copy()
     items = []
+    if return_option:
+        options.append("Go back")
     for item in options:
         if type(item) != str:
             raise TypeError("Items in list must be type str")
@@ -61,7 +69,9 @@ class ship():
         self.cargo_max_weight = cargo_max_weight
         self.cargo = cargo
         self.cargo_weight = 0
+        self.storage = Storage(f"{name} Cargo", cargo_max_weight)
     def calc_cargo(self):
+        self.cargo_weight = 0
         for crate in self.cargo:
             self.cargo_weight += crate.weight
     def show_invent(self):
@@ -81,20 +91,67 @@ class ship():
     def remove_cargo(self,crate_to_remove:crate):
         try:
             self.cargo.remove(crate_to_remove)
+            self.calc_cargo()
             return
         except Exception:
             return "That couldn't be removed"
     def select_from_invent(self):
+        self.calc_cargo()
         self.show_invent()
         print(f"[{len(self.cargo)+1}] | Go back")
         while True:
             try:
                 answer = int(input(f"|:"))
-                if answer <= (len(self.cargo)+1) & answer > 0:
+                if answer <= (len(self.cargo)+1) and answer > 0:
                     return int(answer)
             except Exception as e:
                 #print(e) #Uncomment this line to show error message when the user enters an invalid option
                 print("Invalid selection, try again")
+
+class Storage():
+    def __init__(self,name:str,cargo_max_weight:int = 1000,cargo:list[crate] = []):
+        self.name = name
+        self.cargo_max_weight = cargo_max_weight
+        self.cargo = cargo
+        self.cargo_weight = 0
+    def calc_cargo(self):
+        self.cargo_weight = 0
+        for crate in self.cargo:
+            self.cargo_weight += crate.weight
+    def show_invent(self):
+        self.calc_cargo()
+        print(f"|{self.name} inventory | {self.cargo_weight}lbs/{self.cargo_max_weight}lbs |")
+        i=1
+        for crate in self.cargo:
+            print(f"[{i}] | Crate of {crate.good.name} | Amount: {crate.amount} | Value: ${crate.good.value*crate.amount} | Weight: {crate.weight}")
+            i+=1
+    def add_to_cargo(self,new_crate:crate):
+        self.calc_cargo
+        if self.cargo_weight + new_crate.weight <= self.cargo_max_weight:
+            self.cargo.append(new_crate)
+            return #f"Added {new_crate.good.name}!"
+        else:
+            return "Sorry, that couldnt be added"
+    def remove_cargo(self,crate_to_remove:crate):
+        try:
+            self.cargo.remove(crate_to_remove)
+            self.calc_cargo()
+            return
+        except Exception:
+            return "That couldn't be removed"
+    def select_from_invent(self):
+        self.calc_cargo()
+        self.show_invent()
+        print(f"[{len(self.cargo)+1}] | Go back")
+        while True:
+            try:
+                answer = int(input(f"|:"))
+                if answer <= (len(self.cargo)+1) and answer > 0:
+                    return int(answer)
+            except Exception as e:
+                #print(e) #Uncomment this line to show error message when the user enters an invalid option
+                print("Invalid selection, try again")
+
 class port():
     def __init__(self,name:str,ships:list[ship]):
         self.ships = ships
@@ -103,32 +160,44 @@ class port():
         for ship in ships:
             self.ship_names.append(ship.name)
     def manageGoods(self):
-        print(f"Welcome to port {self.name}")
-        selected_ship:ship = self.ships[menu("Owned ships",self.ship_names) -1]
-        print(f"|{selected_ship.name}|")
-        action = menu("Actions",["Load","Unload","Change name","Go back"])
-        #Loading logic
-        if action == 1:
-            selected_ship_from:ship = self.ships[menu("Load from",self.ship_names) -1] #Get the ship we are moving from
+        while True:
+            clear_terminal()
+            print(f"Welcome to {self.name}")
+            try:
+                selected_ship:ship = self.ships[menu("Owned ships",self.ship_names,return_option=True) -1] #Select a ship to manage\
+            except Exception:
+                break
             while True:
-                try:
-                    moving_cargo = selected_ship_from.cargo[selected_ship_from.select_from_invent() -1] #Select the cargo that will be moved
-                except Exception:
+                clear_terminal()
+                print(f"|{selected_ship.name}|")
+                action = menu("Actions",["Load","Unload","Change name"],True)
+                #Loading logic
+                if action == 1:
+                    clear_terminal()
+                    selected_ship_from:ship = self.ships[menu("Load from",self.ship_names) -1] #Get the ship we are moving from
+                    while True:
+                        try:
+                            clear_terminal()
+                            moving_cargo = selected_ship_from.cargo[selected_ship_from.select_from_invent() -1] #Select the cargo that will be moved
+                        except Exception:
+                            print("Returning...")
+                            break
+                        if selected_ship_from.remove_cargo(moving_cargo): #Attempt to remove cargo from the FROM ship
+                            print("There was a problem moving that cargo") #On failure, do nothing
+                        else: 
+                            if selected_ship.add_to_cargo(moving_cargo): #Attempt to add cargo to current ship
+                                selected_ship_from.add_to_cargo(moving_cargo) #On failure, return removed cargo
+                                print("There was a problem moving that cargo")
+                            else:
+                                print("Cargo moved!")
+                #Unloading logic:
+                elif action == 2:
+                    pass
+                #Change name logic
+                elif action == 3:
+                    pass
+                elif action == 4:
                     break
-                if selected_ship_from.remove_cargo(moving_cargo): #Attempt to remove cargo from the FROM ship
-                    print("There was a problem moving that cargo") #On failure, do nothing
-                else: 
-                    if selected_ship.add_to_cargo(moving_cargo): #Attempt to add cargo to current ship
-                        selected_ship_from.add_to_cargo(moving_cargo) #On failure, return removed cargo
-                        print("There was a problem moving that cargo")
-                    else:
-                        print("Cargo moved!")
-        #Unloading logic:
-        elif action == 2:
-            pass
-        #Change name logic
-        elif action == 3:
-            pass
 
 
 class fleet():
--- tradeGame.py
+++ tradeGame.py
@@ -5,4 +5,18 @@ gold_chest = components.crate(gold,100)
 theSliver = components.ship("the Sliver",cargo=[gold_chest])
 theSplinter = components.ship("the Splinter",cargo=[gold_chest])
 portGrandure = components.port("port Grandure",[theSliver,theSplinter])
-portGrandure.manageGoods()
+
+day = 0
+
+#Game loop
+while True:
+    components.clear_terminal()
+    print(f"Day {day}")
+    answer = components.menu("Main Menu",["Manage goods at port","Quit game"])
+    match answer:                           # <─ use match instead of “case answer:”
+        case 1:                              # option 1
+            portGrandure.manageGoods()
+        case 2:                              # option 3
+            print("Thanks for playing!")
+            break                             # assuming this is inside a loop
+    day += 1
